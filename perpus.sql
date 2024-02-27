47102af0abdcdcc294b767f88de187b602ffb908
kacang <85505098+makankacang@users.noreply.github.com>
2024-02-27 23:10:21 +0700

    Add files via upload

---
+++ perpus.sql
@@ -0,0 +1,528 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 27, 2024 at 04:09 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `BukuID` int NOT NULL,
+  `Judul` varchar(255) NOT NULL,
+  `Penulis` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `dipinjam` int NOT NULL DEFAULT '0',
+  `Penerbit` varchar(255) NOT NULL,
+  `TahunTerbit` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`BukuID`, `Judul`, `Penulis`, `image`, `dipinjam`, `Penerbit`, `TahunTerbit`) VALUES
+(8, 'reer', 'erer', 'Zcznrb9LjH25AZa7pnGZFYSdYVMiIxkWKYo3qb7q.jpg', 0, 'gfhfgh', 3434);
+
+--
+-- Triggers `buku`
+--
+DELIMITER $$
+CREATE TRIGGER `update_buku_id` AFTER UPDATE ON `buku` FOR EACH ROW BEGIN
+    -- Perbarui nilai BukuID di tabel 'KoleksiPribadi'
+    UPDATE `KoleksiPribadi`
+    SET `BukuID` = NEW.`BukuID`
+    WHERE `BukuID` = OLD.`BukuID`;
+
+    -- Perbarui nilai BukuID di tabel 'Peminjaman'
+    UPDATE `Peminjaman`
+    SET `BukuID` = NEW.`BukuID`
+    WHERE `BukuID` = OLD.`BukuID`;
+
+    -- Perbarui nilai BukuID di tabel 'UlasanBuku'
+    UPDATE `UlasanBuku`
+    SET `BukuID` = NEW.`BukuID`
+    WHERE `BukuID` = OLD.`BukuID`;
+
+    -- Perbarui nilai BukuID di tabel 'KategoriBuku_relasi'
+    UPDATE `KategoriBuku_relasi`
+    SET `BukuID` = NEW.`BukuID`
+    WHERE `BukuID` = OLD.`BukuID`;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint UNSIGNED NOT NULL,
+  `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribuku`
+--
+
+CREATE TABLE `kategoribuku` (
+  `KategoriID` int NOT NULL,
+  `NamaKategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kategoribuku`
+--
+
+INSERT INTO `kategoribuku` (`KategoriID`, `NamaKategori`) VALUES
+(10, 'Fantasy');
+
+--
+-- Triggers `kategoribuku`
+--
+DELIMITER $$
+CREATE TRIGGER `update_kategori_id` BEFORE UPDATE ON `kategoribuku` FOR EACH ROW BEGIN
+    UPDATE `KategoriBuku_relasi`
+    SET `KategoriID` = NEW.`KategoriID`
+    WHERE `KategoriID` = OLD.`KategoriID`;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribuku_relasi`
+--
+
+CREATE TABLE `kategoribuku_relasi` (
+  `KategoriBukuID` int NOT NULL,
+  `BukuID` int NOT NULL,
+  `KategoriID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kategoribuku_relasi`
+--
+
+INSERT INTO `kategoribuku_relasi` (`KategoriBukuID`, `BukuID`, `KategoriID`) VALUES
+(5, 8, 10);
+
+--
+-- Triggers `kategoribuku_relasi`
+--
+DELIMITER $$
+CREATE TRIGGER `update_kategori_relasi_buku_id` BEFORE UPDATE ON `kategoribuku_relasi` FOR EACH ROW BEGIN
+    UPDATE `KategoriBuku_relasi`
+    SET `BukuID` = NEW.`BukuID`
+    WHERE `BukuID` = OLD.`BukuID`;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `koleksipribadi`
+--
+
+CREATE TABLE `koleksipribadi` (
+  `KoleksiID` int NOT NULL,
+  `UserID` int NOT NULL,
+  `BukuID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2014_10_12_100000_create_password_resets_table', 2),
+(6, '2024_02_20_104437_add_admin_to_users_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `PeminjamanID` int NOT NULL,
+  `UserID` int NOT NULL,
+  `BukuID` int NOT NULL,
+  `TanggalPeminjaman` date NOT NULL,
+  `TanggalPengembalian` date NOT NULL,
+  `StatusPeminjaman` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Triggers `peminjaman`
+--
+DELIMITER $$
+CREATE TRIGGER `update_peminjaman_user_id` BEFORE UPDATE ON `peminjaman` FOR EACH ROW BEGIN
+    UPDATE `Peminjaman`
+    SET `UserID` = NEW.`UserID`
+    WHERE `UserID` = OLD.`UserID`;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ulasanbuku`
+--
+
+CREATE TABLE `ulasanbuku` (
+  `UlasanID` int NOT NULL,
+  `UserID` int NOT NULL,
+  `BukuID` int NOT NULL,
+  `Ulasan` text NOT NULL,
+  `Rating` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Triggers `ulasanbuku`
+--
+DELIMITER $$
+CREATE TRIGGER `update_ulasanbuku_user_id` BEFORE UPDATE ON `ulasanbuku` FOR EACH ROW BEGIN
+    UPDATE `UlasanBuku`
+    SET `UserID` = NEW.`UserID`
+    WHERE `UserID` = OLD.`UserID`;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `NamaLengkap` varchar(255) NOT NULL,
+  `Alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `level` enum('user','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `level`) VALUES
+(1, 'gagas', 'gagas@gmail.com', NULL, '$2y$12$6fzeTVM2woQHsGrzKY5A0uE/N5/StpeeP5nrpeN3Y3D/xp4zRjZHe', 'mCQLERdm9bi6y7n0X9pyQgkz1oFxmq6Z8CMSoQRlrin1w5KERvkPhRqujtsF', '2024-02-06 21:07:31', '2024-02-06 21:07:31', 'user'),
+(2, '2', '2@gmail.com', NULL, '$2y$12$Wsao8SnUU1OInc/M6kpgGeo/E9pEkQHKwcQEoRlL3nqikSxl2biMW', NULL, '2024-02-07 01:05:42', '2024-02-07 01:05:42', 'admin'),
+(3, 'weq', 'ewq@gmail.com', NULL, '$2y$12$NKcyleaQLMTu61d.OE9LS.H97zI1MBWrtL6up0eZLaiA7pNqeg.Z.', NULL, '2024-02-07 01:26:27', '2024-02-07 01:26:27', 'user'),
+(4, 'gaga', 'gaga@gmail.com', NULL, '$2y$12$UPZvNKwV0KyMiB0K6jlYgu0UVbZqkebnQF2bW61S8Zh/.Fl2Gnvzm', NULL, '2024-02-20 03:39:20', '2024-02-20 03:39:20', 'user'),
+(5, 'gagad', 'gagad@gmail.com', NULL, '$2y$12$RT2Op2IEh62M7JxoDG8Us.OG0IUiNEuZB05FRAlCd.Rnm91p3gWb.', NULL, '2024-02-20 04:27:32', '2024-02-20 04:27:32', 'admin'),
+(6, 'adad', 'adad@gmail.com', NULL, '$2y$12$wXyBB4UGBvZsSSsOFE7dCe6y7RRA7PE9HkvTXAhIhBd1u3ykrApwK', NULL, '2024-02-20 04:29:33', '2024-02-20 04:29:33', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`BukuID`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `kategoribuku`
+--
+ALTER TABLE `kategoribuku`
+  ADD PRIMARY KEY (`KategoriID`);
+
+--
+-- Indexes for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  ADD PRIMARY KEY (`KategoriBukuID`),
+  ADD KEY `kategoribuku_relasi_ibfk_1` (`BukuID`),
+  ADD KEY `kategoribuku_relasi_ibfk_2` (`KategoriID`);
+
+--
+-- Indexes for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD PRIMARY KEY (`KoleksiID`),
+  ADD KEY `koleksipribadi_ibfk_2` (`BukuID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`PeminjamanID`),
+  ADD KEY `peminjaman_ibfk_2` (`BukuID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `ulasanbuku`
+--
+ALTER TABLE `ulasanbuku`
+  ADD PRIMARY KEY (`UlasanID`),
+  ADD KEY `ulasanbuku_ibfk_2` (`BukuID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `BukuID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategoribuku`
+--
+ALTER TABLE `kategoribuku`
+  MODIFY `KategoriID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  MODIFY `KategoriBukuID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  MODIFY `KoleksiID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `PeminjamanID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ulasanbuku`
+--
+ALTER TABLE `ulasanbuku`
+  MODIFY `UlasanID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  ADD CONSTRAINT `kategoribuku_relasi_ibfk_1` FOREIGN KEY (`BukuID`) REFERENCES `buku` (`BukuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kategoribuku_relasi_ibfk_2` FOREIGN KEY (`KategoriID`) REFERENCES `kategoribuku` (`KategoriID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD CONSTRAINT `koleksipribadi_ibfk_2` FOREIGN KEY (`BukuID`) REFERENCES `buku` (`BukuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `koleksipribadi_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`BukuID`) REFERENCES `buku` (`BukuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peminjaman_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `ulasanbuku`
+--
+ALTER TABLE `ulasanbuku`
+  ADD CONSTRAINT `ulasanbuku_ibfk_2` FOREIGN KEY (`BukuID`) REFERENCES `buku` (`BukuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ulasanbuku_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
